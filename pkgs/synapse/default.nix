7a24c392ff1f30534649f6fe38ef80ad0f78b649
borgstad <43218087+borgstad@users.noreply.github.com>
2022-02-19 10:05:05 +0100

    Add ssh ymir (#13)
    * Add SSH to Ymirc

---
+++ pkgs/synapse/default.nix
@@ -0,0 +1,32 @@
+{config, pkgs, lib, ...}:
+# {
+#  services.nginx = {
+#    enable = true;
+#    virtualHosts = {
+
+#      ## virtual host for Syapse
+#      "matrix.borgstad.dk" = {
+#        ## for force redirecting HTTP to HTTPS
+#        forceSSL = true;
+#        ## this setting takes care of all LetsEncrypt business
+#        enableACME = true;
+#        locations."/" = { 
+#         proxyPass = "http://localhost:8008";
+#        };
+#      };
+#    };
+
+#    ## other nginx specific best practices
+#    recommendedGzipSettings = true;
+#    recommendedOptimisation = true;
+#    recommendedTlsSettings = true;
+#  };
+# }
+{
+  services.nginx.enable = true;
+  services.nginx.virtualHosts."localhost" = {
+    addSSL = false;
+    enableACME = false;
+    root = "/var/www/test";
+  };
+}
--- profiles/ymir-server.nix
+++ profiles/ymir-server.nix
@@ -3,6 +3,7 @@
 with lib;
   # Enable the X11 windowing system.
 
+
 {
   environment.systemPackages = with pkgs; [
     alacritty
@@ -37,6 +38,7 @@ with lib;
     ../machines/jotunheim/network.nix
     ../pkgs/bash
     ../pkgs/ssh-server
+    ../pkgs/synapse
     ../pkgs/xserver
     ../pkgs/plex
     ../pkgs/deluge
@@ -55,4 +57,19 @@ with lib;
     hashedPasswordPath = "";
     sshAuthKeysPath = [ "" ];
   };
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+        if (action.id == "org.freedesktop.login1.suspend" ||
+            action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.hibernate" ||
+            action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
+        {
+            return polkit.Result.NO;
+        }
+    });
+  '';
 }
