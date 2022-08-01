d86a3c104b1ed18f503856692f5d9c35f0494650
ymir <aborgstad@gmail.com>
2022-08-01 14:20:52 +0200

    chore: extra settings for Deluge

--- pkgs/deluge/default.nix
+++ pkgs/deluge/default.nix
@@ -5,8 +5,27 @@
     enable = true;
     web.enable = true;
     web.openFirewall = true;
-    dataDir = "/plex/videos";
-    user = "plex";
+    openFirewall = true;
+    declarative = true;
+    config = {
+      download_location = "/plex/videos";
+      sequential_download = true;
+      allow_remote = true;
+      max_upload_speed = "500.0";
+      daemon_port = 58846;
+      listen_ports = [ 6881 6889 ];
+    };
+    authFile = builtins.toString ./authfile;
+    user = "deluge";
     group = "plex";
+  };
+  networking.firewall = {
+    enable = true;
+    allowPing = false;
+    allowedTCPPorts = [
+      6881
+      6889
+      58846
+    ];
   };
 }
