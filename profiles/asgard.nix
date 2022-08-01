e977ab8a2ac7df5e9ea3b029f0635a7b7680315f
Odin <aborgstad@gmail.com>
2022-08-01 14:48:58 +0200

    chore: update asgard profile

--- profiles/asgard.nix
+++ profiles/asgard.nix
@@ -7,25 +7,31 @@ with lib;
   environment.systemPackages = with pkgs; [
     alacritty
     aspell
+    anki
     aspellDicts.en
     bash
     ctags
     curl
+    graphviz
     element-desktop
-    emacs
     firefox
+    gcc
     gitAndTools.gitFull
     gnumake
     htop
-    nix-prefetch-scripts
     mkpasswd
+    nix-prefetch-scripts
+    nodejs
     openssl
     python37
     slack
+    sqlite
+    okular
     stdenv
     strace
     sudo
     sysstat
+    go
     tmux
     unzip
     vim
@@ -36,21 +42,39 @@ with lib;
   imports = [
     ../machines/asgard/hardware-configuration.nix
     ../machines/asgard/network.nix
-    ../pkgs/bash
+    # ../pkgs/bash
+    ../pkgs/zsh
+    ../pkgs/emacs
     ../pkgs/xserver
-    # ../pkgs/emacs
     ../modules/syncthing
     ../modules/users/standard-user.nix
   ];
+
+  (final: prev: {
+  anki = prev.anki.overrideAttrs (oldAttrs: rec {
+    version = "2.1.28";
+    pname = oldAttrs.pname;
+    src = prev.fetchurl {
+      urls = [
+        "https://github.com/ankitects/anki/archive/${version}.tar.gz"
+      ];
+      sha256 = "0000000000000000000000000000000000000000000000000000";
+    };
+  });
+  })
+
   services.borgstadSyncthing = {
     enable = true;
     user = "odin";
   };
-  
   services.borgstadUser = {
     user = "odin";
     isAdmin = true;
     hashedPasswordPath = "";
     sshAuthKeysPath = [ "" ];
   };
+  virtualisation.docker.enable = true;
+  users.users.odin.extraGroups = [ "docker" ];
+  users.users.odin.shell = pkgs.zsh;
+  users.defaultUserShell = pkgs.zsh;
 }
