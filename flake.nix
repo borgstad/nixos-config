677ee0101c90c48e47dab9bd11da399c1a2b1d54
ymir <aborgstad@gmail.com>
2023-05-21 10:06:39 +0200

    rebase: test

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "NixOS config";
 
   inputs = {
-    # Software inputs
+    # App inputs
     nixos-vscode-server.url = "github:nix-community/nixos-vscode-server";
-  };
+   };
 
   outputs = inputs@{ self, ... }:
     inputs.flake-parts.lib.mkFlake { inherit (inputs) self; } {
       systems = [ "x86_64-linux" ];
       flake = {
+        };
       };
-    };
 }
