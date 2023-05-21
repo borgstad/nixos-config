98ead112d753fae4e60803bb02c8dd481b70626c
ymir <aborgstad@gmail.com>
2023-05-21 10:24:28 +0200

    rebase: test

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = {
     # App inputs
     nixos-vscode-server.url = "github:nix-community/nixos-vscode-server";
-    nixpkgs.url = "nixpkgs/nixos-22.11";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:nixpkgs/nixos-unstable";
    };
 
   outputs = inputs@{ self, ... }:
