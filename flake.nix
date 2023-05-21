2de587a19b5f60c17e3d3e0f9011cb68c8555572
ymir <aborgstad@gmail.com>
2023-05-21 10:52:30 +0200

    rebase: test

--- flake.nix
+++ flake.nix
@@ -5,28 +5,16 @@
     # App inputs
     nixos-vscode-server.url = "github:nix-community/nixos-vscode-server";
     nixpkgs.url = "nixpkgs/nixos-22.11";
-    # nixpkgs-unstable.url = "github:nixpkgs/nixos-unstable";
   };
 
   outputs = inputs@{ self, ... }:
     let
       systems = [ "x86_64-linux" ];
-      # overlay-unstable = final: prev: {
-      #   unstable = nixpkgs-unstable.legacyPackages.${prev.system};
-      # };
     in {
       inherit systems;
       modules = [
-        # ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
         ./configuration.nix
+        ./profiles
       ];
-      # nixosConfigurations.jotunheim = nixpkgs.lib.nixosSystem {
-      #   modules = [
-      #     vscode-server.nixosModules.default
-      #     ({ config, pkgs, ... }: {
-      #       services.vscode-server.enable = true;
-      #     })
-      #   ];
-      # };
     };
 }
