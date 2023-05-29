9f46c61654b18d1abb51246a0be69ac14efa752e
Surt <aborgstad@gmail.com>
2023-05-29 11:55:46 +0000

    feat: enable bluetooth, gimle

--- machines/gimle/hardware-configuration.nix
+++ machines/gimle/hardware-configuration.nix
@@ -37,6 +37,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  hardware.bluetooth.enable = true;
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
