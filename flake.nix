302629b01a5e5bacc96450d5c9f7d2be57293751
Andreas Borgstad <aborgstad@gmail.com>
2023-10-28 17:47:50 +0200

    update

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "NixOS config";
+
+  inputs = {
+    # App inputs
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    agenix.url = "github:ryantm/agenix";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    ssh-keys = {
+      url = "https://github.com/borgstad.keys";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, agenix, vscode-server, ... }:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations =  {
+        gimle = lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            ./profiles/gimle.nix
+          ];
+        };
+        muspel = lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            ./profiles/muspel.nix
+          ];
+        };
+
+        jotunheim = lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            ./profiles/jotunheim.nix
+            # How to move this into a seperate file?
+            vscode-server.nixosModules.default
+            ({ config, pkgs, ... }: {
+              services.vscode-server.enable = true;
+            })
+          ];
+        };
+      };
+    };
+}
